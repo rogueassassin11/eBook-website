93a2725abd254593a0ef7209e508193fbb2b6252
rogueassassin11 <maecagadas2011@gmail.com>
2023-12-01 22:26:05 +0800

    Finished Details, Testimonials, CTA, and Footer

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-// Add custom JavaScript here
+// Navbar changing color when scrolling
+
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+
+  // add event listener to listen on scroll effect
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("bg-dark");
+      navbar.classList.add("navbar-sticky");
+    } else {
+      navbar.classList.remove("bg-dark");
+      navbar.classList.remove("navbar-sticky");
+    }
+  });
+}
+
+document.addEventListener("DOMContentLoaded", userScroll);
